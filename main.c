aba81d2ec63401e9bf64f261afca26704ce5f445
Ameilorate <ameilorate2@gmail.com>
2018-02-03 20:27:43 -0800

    Make nmfd exit if another program has control over the mouse files

--- main.c
+++ main.c
@@ -5,11 +5,12 @@
 #include <sys/select.h>
 #include <linux/input.h>
 #include <unistd.h>
+#include <errno.h>
 
 #define DEVICES_COUNT 8
 #define OFFSET 262
 
-void rd_error(void);
+void rd_error(char *action, char *hint);
 
 const char *devices[DEVICES_COUNT][3] = {
         {"Naga Epic",
@@ -67,7 +68,14 @@ int main(int argc, char **argv)
 
         if (grab) {
                 // Give application exclusive control over side buttons.
-                ioctl(sidefd, EVIOCGRAB, 1);
+                if (ioctl(sidefd, EVIOCGRAB, 1) == -1) {
+                        if (errno == EBUSY)
+                                rd_error("grabbing side buttons",
+                                         "Another program may be grabbing the side buttons.\n"
+                                         "You'll have to kill that program before running nmfd.");
+                        else
+                                rd_error("grabbing side buttons", NULL);
+                }
         }
 
         while (1) {
@@ -90,7 +98,7 @@ int main(int argc, char **argv)
                 if (FD_ISSET(sidefd, &readset)) {       // Side buttons
                         rd1 = read(sidefd, ev1, sizeof(ev1));
                         if (rd1 == -1)
-                                rd_error();
+                                rd_error("reading side buttons", NULL);
                         if (ev1[1].value > 1 || ev1[1].value < 0)       // Irrelevant stuff
                                 goto unimportant;
                         if (ev1[1].code == 0) { // Button is held.
@@ -103,7 +111,7 @@ int main(int argc, char **argv)
                 } else if (FD_ISSET(extrafd, &readset)) {       // Extra Buttons
                         rd2 = read(extrafd, ev2, sizeof(ev2));
                         if (rd2 == -1)
-                                rd_error();
+                                rd_error("reading extra buttons", NULL);
                         if (ev2[1].value > 1 || ev2[1].code < OFFSET || ev2[1].code < (OFFSET + 13) || ev2[1].value < 0)        // Irrelevant stuff
                                 goto unimportant;
                         button_num = ev2[1].code - OFFSET;
@@ -119,10 +127,14 @@ int main(int argc, char **argv)
         return 0;
 }
 
-void rd_error(void)
+void rd_error(char *action, char *hint)
 {
-        fprintf(stderr, "Error: %m\n");
-        exit(2);
+        fprintf(stderr, "Error while %s: %m\n", action);
+        if (hint) {
+                fputs(hint, stderr);
+                fputc('\n', stderr);
+        }
+        exit(1);
         // In the future we might try to restart, in case of the condition that the mouse was unplugged for a short time.
         // For now exiting is good.
 }
